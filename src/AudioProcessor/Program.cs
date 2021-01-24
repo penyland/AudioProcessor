556e6d0dc4d3a3cc97a40ba23d7f79b8be5389b7
Peter Nylander <peter.nylander@gmail.com>
2021-01-24 20:09:24 +0100

    First commit

---
+++ src/AudioProcessor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AudioProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
